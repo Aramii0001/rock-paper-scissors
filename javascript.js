4391b604bb40ec4cac29dadbe76d65f832e074c1
Aramii <Vincent.Niedermayer@gmail.com>
2023-02-18 17:53:39 -0500

    Finish intro sequence

--- javascript.js
+++ javascript.js
@@ -4,6 +4,13 @@ const result = document.querySelector('.winner');
 const choiceBtns = document.querySelectorAll('.btn');
 const replayBtn = document.createElement("span");
 const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const one = document.getElementById('1');
+const two = document.getElementById('2');
+const three = document.getElementById('3');
+const four = document.getElementById('4');
+const five = document.getElementById('5');
+const terminalContainer = document.querySelector('.container')
+const introBox = document.querySelector('.intro')
 let player_score = 0;
 let computer_score = 0;
 
@@ -64,14 +71,16 @@ function pickWinner(){
 }
 
 function playAgain(){
-    replayBtn.textContent = 'replay'
+    replayBtn.textContent = 'replay';
     result.append(replayBtn);
     //remove buttons
     choiceBtns.forEach(button => button.setAttribute('disabled', ''));
 }
 
 choiceBtns.forEach(button => button.addEventListener("click", () => {
+    console.log('clicked');
     player_choice = button.textContent.toLowerCase();
+    console.log(player_choice);
     playRound(player_choice);
 }));
 
@@ -105,7 +114,8 @@ document.querySelector('h1').onmouseover =
 //thinkin we get each p element and on page load we do that event for each, 
 //waiting 5 seconds before removing the element
 
-document.getElementById('1').onanimationstart=
+//1
+one.onanimationstart=
 event => {
     let iterations = 0;
 
@@ -125,7 +135,98 @@ event => {
         iterations += 1;
     }, 30);
 }
+one.onanimationend = () => {one.style.display = "none";}
+//2
+two.onanimationstart=
+event => {
+    let iterations = 0;
 
-const len = document.getElementById('1');
-console.log(len.innerHTML.length);
+    const interval = setInterval(() => {
+    event.target.innerText = event.target.innerText.split("")
+    .map((letter, index) => {
+        if(index < iterations){
+            return event.target.dataset.value[index];
+        }
+
+        return letters[Math.floor(Math.random() * 26)]
+    })
+    .join("");
+
+        if(iterations >= 76) clearInterval(interval);
+        
+        iterations += 1;
+    }, 30);
+}
+two.onanimationend = () => {two.style.display = "none";}
+//3
+three.onanimationstart=
+event => {
+    let iterations = 0;
+
+    const interval = setInterval(() => {
+    event.target.innerText = event.target.innerText.split("")
+    .map((letter, index) => {
+        if(index < iterations){
+            return event.target.dataset.value[index];
+        }
+
+        return letters[Math.floor(Math.random() * 26)]
+    })
+    .join("");
+
+        if(iterations >= 72) clearInterval(interval);
+        
+        iterations += 1;
+    }, 30);
+}
+three.onanimationend = () => {three.style.display = "none";}
+//4
+four.onanimationstart=
+event => {
+    let iterations = 0;
+
+    const interval = setInterval(() => {
+    event.target.innerText = event.target.innerText.split("")
+    .map((letter, index) => {
+        if(index < iterations){
+            return event.target.dataset.value[index];
+        }
+
+        return letters[Math.floor(Math.random() * 26)]
+    })
+    .join("");
+
+        if(iterations >= 26) clearInterval(interval);
+        
+        iterations += 1;
+    }, 30);
+}
+four.onanimationend = () => {four.style.display = "none";}
+//5
+five.onanimationstart=
+event => {
+    let iterations = 0;
+
+    const interval = setInterval(() => {
+    event.target.innerText = event.target.innerText.split("")
+    .map((letter, index) => {
+        if(index < iterations){
+            return event.target.dataset.value[index];
+        }
+
+        return letters[Math.floor(Math.random() * 26)]
+    })
+    .join("");
+
+        if(iterations >= 55) clearInterval(interval);
+        
+        iterations += 1;
+    }, 30);
+}
+five.onanimationend = () => {
+    five.style.display = "none";
+    terminalContainer.style.visibility = "visible";
+    introBox.style.display = "none";
+}
+
 //remember make a termina you can type into ok bye 
